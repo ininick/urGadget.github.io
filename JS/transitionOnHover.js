0c51d358095faa0c8da75a591b6d4718a5c3d7c4
RiHuuang <richardhuang.email@gmail.com>
2023-05-03 16:32:41 +0700

    kelarin home tanpa footer

---
+++ JS/transitionOnHover.js
@@ -0,0 +1,13 @@
+// const ptProduct = document.querySelector('.pt-product');
+// const image = document.querySelector('.pt-product-img');
+// const desc = document.querySelector('.pt-product-desc');
+
+// ptProduct.addEventListener("mouseover",()=>{
+//     image.style.transform = "scale(0.8)";
+//     desc.style.display = "none";
+// });
+
+// ptProduct.addEventListener("mouseout",()=>{
+//     image.style.transform = "scale(1)";
+//     desc.style.display = "block";
+// });
